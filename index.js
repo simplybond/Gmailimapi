d03d8a4dc7d7f004d877677f18d68546fd66e568
simplybond <124093129+simplybond@users.noreply.github.com>
2025-01-28 07:36:35 +1000

    тест

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ if (!botToken) {
     process.exit(1);
 }
 
+// Создание экземпляра бота с debug и polling
 const bot = new TelegramBot(botToken, { polling: true });
 
 bot.getMe()
@@ -21,9 +22,16 @@ bot.getMe()
         process.exit(1);
     });
 
+// Глобальный обработчик для всех событий (для отладки)
+bot.on('message', (msg) => {
+    console.log(`Получено сообщение от ${msg.chat.id}: ${msg.text}`);
+    bot.sendMessage(msg.chat.id, `Вы отправили: ${msg.text}`);
+});
+
 // Обработчик команды /start
 bot.onText(/\/start/, (msg) => {
     const chatId = msg.chat.id;
+    console.log('Команда /start получена');
     bot.sendMessage(chatId, 'Привет! Нажми кнопку ниже:', {
         reply_markup: {
             inline_keyboard: [[{ text: 'Проверить письма', callback_data: 'check_emails' }]],
@@ -33,6 +41,33 @@ bot.onText(/\/start/, (msg) => {
 
 // Обработчик callback_query
 bot.on('callback_query', (query) => {
-    console.log('Получен callback_query:', query);
-    bot.answerCallbackQuery(query.id, { text: 'Кнопка нажата!' });
+    console.log(`Получен callback_query с данными: ${query.data}`);
+    bot.answerCallbackQuery(query.id, { text: 'Кнопка нажата!' })
+        .then(() => {
+            console.log('Ответ на callback_query отправлен');
+        })
+        .catch((err) => {
+            console.error('Ошибка отправки ответа на callback_query:', err);
+        });
+
+    bot.sendMessage(query.message.chat.id, 'Вы нажали кнопку!');
 });
+
+// Обработчик ошибок
+bot.on('polling_error', (err) => {
+    console.error('Ошибка polling:', err.code, err.message);
+});
+
+bot.on('webhook_error', (err) => {
+    console.error('Ошибка webhook:', err.message);
+});
+
+// Дополнительные логирования на случай неожиданных событий
+bot.on('error', (err) => {
+    console.error('Общая ошибка бота:', err.message);
+});
+
+bot.on('callback_query_error', (err) => {
+    console.error('Ошибка в обработчике callback_query:', err.message);
+});
+
